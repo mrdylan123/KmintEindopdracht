d9049e1b341ec6b130990025d1a6a0533b6f07c6
DennisvHest <dennisvhest@hotmail.nl>
2018-12-21 11:44:29 +0100

    Added distance class

---
+++ pigisland/src/kmint/pigisland/distance.cpp
@@ -0,0 +1,25 @@
+#include "../../../include/kmint/pigisland/distance.h"
+
+distance::distance() : shortestDistance_{ std::numeric_limits<int>::max() }, fromNode_{ nullptr }
+{
+}
+
+int distance::shortestDistance() const
+{
+    return shortestDistance_;
+}
+
+kmint::graph::basic_node<kmint::map::map_node_info>* distance::fromNode() const
+{
+    return fromNode_;
+}
+
+void distance::setShortestDistance(int shortestDistance)
+{
+    shortestDistance_ = shortestDistance;
+}
+
+void distance::setFromNode(kmint::graph::basic_node<kmint::map::map_node_info>* fromNode)
+{
+    fromNode_ = fromNode;
+}
